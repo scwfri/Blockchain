b5a3bcba6ca79225becbba5dd18f605ba742abea
sfrieds3 <sfrieds3@gmail.com>
2018-02-09 22:33:58 -0600

    wip

--- src/Blockchain.java
+++ src/Blockchain.java
@@ -65,35 +65,82 @@ import java.util.concurrent.*;
 
 
 class BlockchainNode {
-    private UnverifiedBlockchain unverifiedBlockChain;
-    private Blockchain blockchain;
-    private BlockingQueue<String> unverifiedQueue;
-    private int privateKey;
+    private UnverifiedBlockClient unverifiedBlockClient; // client to do "work"
+    private Stack<Blockchain> blockchainStack; // stack to store full blockchain
+    private BlockingQueue<String> unverifiedQueue; // queue of unverified blocks
+    private int privateKey; // private key for server
     private int pid;
     private int blockchainServerPort;
     private int unverifiedBlockServerPort;
     private int publicKeyServerPort;
+
+    BlockchainNode() {
+        // privateKey = Keys.getInstance.getPrivateKey();
+        unverifiedBlockClient = new UnverifiedBlockClient();
+        blockchainStack = new Stack<Blockchain>();
+    }
 }
 
 class Blockchain {
-    private String previousBlock;
-    private String currentBlock;
+    private String previousBlockHash;
+    private String currentBlockHash;
+    private String currentBlockContents;
 
-
+    Blockchain(String prevHash, String newBlockHash, String newBlockContents) {
+        previousBlockHash = prevHash;
+        currentBlockHash = newBlockHash;
+        currentBlockContents = newBlockContents;
+    }
 }
 
 class BlockchainNodeList {
+    // singleton
     private static BlockchainNodeList instance;
-    ArrayList<BlockchainNode> blockchainNodeList;
+    private ArrayList<BlockchainNode> blockchainNodeList;
 
-    Blockchain() {
-        
+    private BlockchainNodeList() {
+        blockchainNodeList = new ArrayList<BlockchainNode>();
     }
 
-    public void getInstance() {
+    public BlockchainNodeList getInstance() {
         if (instance == null) {
             instance = new BlockchainNodeList();
         }
         return instance;
     }
+
+    public void muliCast(String message) {
+        // method to multicast string (i.e. new block, completed block) to all nodes
+    }
+
+    public void addBlockchainNode(BlockchainNode b) {
+        blockchainNodeList.add(b);
+    }
+
+    public ArrayList<BlockchainNode> getBlockchainNodeList() {
+        return blockchainNodeList;
+    }
+}
+
+class UnverifiedBlockClient {
+
+}
+
+class Keys {
+    // singleton
+    // provide public key to all clients
+    // calculate and return new private key to BlockchainNode
+    private static Keys instance;
+
+    private Keys() {
+
+    }
+
+    public Keys getInstance() {
+        if (instance == null) {
+            instance = new Keys();
+        }
+        return instance;
+    }
+
 }
