0660735e6f36316734f5b6631d66ce13b0f4f32d
sfrieds3 <sfrieds3@gmail.com>
2018-02-10 20:32:18 -0600

    udpate all.sh

--- src/all.sh
+++ src/all.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-gnome-terminal -e java Blockchain 0 &
+xterm -hold -e java Blockchain 0 &
 sleep 2 &
-gnome-terminal -e java Blockchain 1 &
+xterm -hold -e java Blockchain 1 &
 sleep 2 &
-gnome-terminal -e java Blockchain 2
+xterm -hold -e java Blockchain 2
